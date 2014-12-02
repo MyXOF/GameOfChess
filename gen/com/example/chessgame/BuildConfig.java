ca7e03fbc609de6f8cf9e752af8ae469ee312d53
lich1288 <lich1288@163.com>
2014-12-02 19:40:09 +0800

    new

--- gen/com/example/chessgame/BuildConfig.java
+++ gen/com/example/chessgame/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.gameofchess;
+package com.example.chessgame;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
